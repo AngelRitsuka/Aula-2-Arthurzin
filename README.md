e723d142ad86b05de8f8d201b28a4d4b72e13dfc
AngelRitsuka <88597612+AngelRitsuka@users.noreply.github.com>
2023-04-19 14:49:20 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Aula-2-Arthurzin
+# BIBLIOTECA-P5-COMPLETA
+Bibliotecas: P5, P5.Play,P5.sound.min,Index.html,Style,CSS
---
+++ sketch.js
@@ -0,0 +1,32 @@
+//variavel - memoria - se lembrar
+
+var arthur;
+var angelica;
+var bola;
+
+//configurações
+function setup()
+{
+    //criar a tela
+    createCanvas(500,500);
+
+    //x,y,largura,altura
+    arthur = createSprite(250,250,50,50);
+
+}
+
+/*
+posso
+escrever
+um
+paragrafo kk
+*/ 
+
+//coisas dentro da tela
+function draw()
+{
+    background("black");
+
+    //todos aparecerem
+    drawSprites();
+}
